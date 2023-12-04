531ae470a87d54ba5c3bcbeb60c8dd2aa5c115a4
권장순 <jsoonworld@gmail.com>
2023-12-04 22:52:17 +0900

    feat( Store) : 매장 오픈, 마감 속성 추가.

--- src/main/java/designPattern/store/model/Store.java
+++ src/main/java/designPattern/store/model/Store.java
@@ -4,10 +4,21 @@ package designPattern.store.model;
 public class Store {
     private String name;
     private int capacity;
+    private boolean isOpen;
 
-    public Store(String name, int capacity) {
+    public Store(String name, int capacity, boolean isOpen) {
         this.name = name;
         this.capacity = capacity;
+        this.isOpen = false;
+    }
+
+    // TODO : 매장에 맞게 오픈, 마감 시간 적용.
+    public void openStore() {
+        isOpen = true;
+    }
+
+    public void closeStore() {
+        isOpen = false;
     }
 
     public String getName() {
