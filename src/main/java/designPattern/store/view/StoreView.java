111e8a90adb898b2416abe604824caa649434d78
권장순 <jsoonworld@gmail.com>
2023-12-04 22:18:54 +0900

    feat( StoreView ) : 상점 정보를 출력하는 view 구현.

---
+++ src/main/java/designPattern/store/view/StoreView.java
@@ -0,0 +1,8 @@
+package designPattern.store.view;
+
+public class StoreView {
+    public void printStoreInfo(String name, int capacity) {
+        System.out.println("상점 이름: " + name);
+        System.out.println("상점 크기: " + capacity);
+    }
+}
