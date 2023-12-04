7ab407d6847f72b4d5bee02cd6513a835855db10
권장순 <jsoonworld@gmail.com>
2023-12-04 23:08:41 +0900

    feat : 파라미터 조정.

--- src/main/java/designPattern/store/model/PCRoomStore.java
+++ src/main/java/designPattern/store/model/PCRoomStore.java
@@ -1,11 +1,11 @@
 package designPattern.store.model;
 
 public class PCRoomStore extends Store {
-    public PCRoomStore(String name, int capacity) {
-        super(name, capacity);
+    public PCRoomStore(String name, int capacity,boolean isOpen) {
+        super(name, capacity, isOpen);
     }
 
     public PCRoomStore() {
-        super("챌린저_PC방", 20);
+        super("챌린저_PC방", 20, false);
     }
 }
--- src/main/java/designPattern/store/model/SportsStore.java
+++ src/main/java/designPattern/store/model/SportsStore.java
@@ -1,11 +1,11 @@
 package designPattern.store.model;
 
 public class SportsStore extends Store {
-    public SportsStore(String name, int capacity) {
-        super(name, capacity);
+    public SportsStore(String name, int capacity, boolean isOpen) {
+        super(name, capacity, isOpen);
     }
 
     public SportsStore() {
-        super("나이키", 10);
+        super("나이키", 10, false  );
     }
 }
