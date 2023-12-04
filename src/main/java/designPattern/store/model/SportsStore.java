1682d28fdcc3ab7e71520cb513bd001b972a02be
권장순 <jsoonworld@gmail.com>
2023-12-04 22:29:53 +0900

    feat( SportsStore ) : 스포츠 매장 구현.

---
+++ src/main/java/designPattern/store/model/SportsStore.java
@@ -0,0 +1,7 @@
+package designPattern.store.model;
+
+public class SportsStore extends Store {
+    public SportsStore(String name, int capacity) {
+        super(name, capacity);
+    }
+}
--- src/main/java/designPattern/store/model/Store.java
+++ src/main/java/designPattern/store/model/Store.java
@@ -5,6 +5,11 @@ public class Store {
     private String name;
     private int capacity;
 
+    public Store(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     public String getName() {
         return name;
     }
