68bd888271d8f98c30d49fd7b79a08455192de92
권장순 <jsoonworld@gmail.com>
2023-12-04 22:53:40 +0900

    feat( LuxuryStore ) : 대기 리스트 기능 추가.

--- src/main/java/designPattern/store/model/LuxuryStore.java
+++ src/main/java/designPattern/store/model/LuxuryStore.java
@@ -1,11 +1,20 @@
 package designPattern.store.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LuxuryStore extends Store {
+    private List<String> waitingList;
     public LuxuryStore(String name, int capacity) {
-        super(name, capacity);
+        super(name, capacity, false);
+        this.waitingList = new ArrayList<>();
+    }
+
+    public void addToWaitingList(String customerName) {
+        waitingList.add(customerName);
     }
 
     public LuxuryStore() {
-        super("에르메스", 5);
+        super("에르메스", 5, false);
     }
 }
