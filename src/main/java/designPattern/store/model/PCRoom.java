9d5883db420d1e943a344da14f777c8bfb8edeee
권장순 <jsoonworld@gmail.com>
2023-12-04 22:31:44 +0900

    refactoring( PCRoomStore ) : 이름 재정의.

--- src/main/java/designPattern/store/model/PCRoom.java
+++
@@ -1,7 +0,0 @@
-package designPattern.store.model;
-
-public class PCRoom extends Store {
-    public PCRoom(String name, int capacity) {
-        super(name, capacity);
-    }
-}
---
+++ src/main/java/designPattern/store/model/PCRoomStore.java
@@ -0,0 +1,7 @@
+package designPattern.store.model;
+
+public class PCRoomStore extends Store {
+    public PCRoomStore(String name, int capacity) {
+        super(name, capacity);
+    }
+}
