b953776adf4aab61a5f8d1dfa1020ea382154c76
권장순 <jsoonworld@gmail.com>
2023-12-05 21:41:38 +0900

    feat( LuxuryStore ) : 럭셔리 매장 객체 기능 구현.

--- src/main/java/designPattern/store/model/stores/LuxuryStore.java
+++ src/main/java/designPattern/store/model/stores/LuxuryStore.java
@@ -2,6 +2,8 @@ package designPattern.store.model.stores;
 
 import designPattern.store.items.Item;
 import designPattern.store.model.interfaces.Store;
+import designPattern.store.model.interfaces.DiscountStrategy;
+import designPattern.store.model.strategies.NoDiscountStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,60 +13,65 @@ public class LuxuryStore implements Store {
     private int capacity;
     private boolean isOpen;
     private List<String> waitingList;
+    private DiscountStrategy discountStrategy;
 
     private int purchaseLimit = 1;
-    private final boolean salePolicy = false;
-
 
     public LuxuryStore(String name, int capacity, boolean isOpen) {
         this.name = name;
         this.capacity = capacity;
         this.isOpen = isOpen;
         this.waitingList = new ArrayList<>();
+        this.discountStrategy = new NoDiscountStrategy();
     }
 
+    // 상품 구매 메소드
     public boolean purchaseItem(Item item, int quantity) {
         if (quantity > purchaseLimit) {
-            System.out.println("구매 제한 수량을 초과하였습니다.");
+            System.out.println("구매 제한 수량을 초과하였습니다: " + purchaseLimit + "개.");
             return false;
         }
+        // 할인 전략을 적용하여 최종 가격 계산
+        double finalPrice = discountStrategy.applyDiscount(item.getPrice() * quantity);
+        System.out.println(item.getName() + " 상품을 " + finalPrice + "원에 구매하셨습니다.");
         return true;
     }
 
+    // 할인 가능 여부 확인 메소드
     public boolean isSaleAllowed() {
-        return salePolicy;
+        System.out.println("럭셔리 매장에서는 할인이 불가능합니다.");
+        return false;
     }
 
+    // 대기 목록에 추가 메소드
     public void addToWaitingList(String customerName) {
         waitingList.add(customerName);
     }
 
     @Override
     public void openStore() {
-        isOpen = true;
-        // 추가적인 오픈 로직...
+        
     }
 
     @Override
     public void closeStore() {
-        isOpen = false;
-        // 추가적인 마감 로직...
+
     }
 
     @Override
     public String getName() {
-        return name;
+        return null;
     }
 
     @Override
     public int getCapacity() {
-        return capacity;
+        return 0;
     }
 
     @Override
     public boolean isOpen() {
-        return isOpen;
+        return false;
     }
-
 }
+
 
