368353f7eb313bd95690404da4aaf93e955a3b7b
권장순 <jsoonworld@gmail.com>
2023-12-05 21:47:54 +0900

    feat( SportsStore ) : 스포츠 매장 구현체 기능 구현.

--- src/main/java/designPattern/store/model/factories/SportsStoreFactory.java
+++ src/main/java/designPattern/store/model/factories/SportsStoreFactory.java
@@ -1,11 +1,17 @@
 package designPattern.store.model.factories;
 
 import designPattern.store.model.interfaces.Store;
+import designPattern.store.model.interfaces.DiscountStrategy;
+import designPattern.store.model.strategies.PercentageDiscountStrategy;
 import designPattern.store.model.stores.SportsStore;
 
 public class SportsStoreFactory extends StoreFactory {
     @Override
     public Store createStore() {
-        return new SportsStore("나이키", 10, false);
+        // 할인 전략 인스턴스 생성, 예를 들어 모든 상품에 대해 10% 할인을 적용한다고 가정
+        DiscountStrategy discountStrategy = new PercentageDiscountStrategy(0.10);
+
+        // SportsStore 인스턴스 생성 시 할인 전략을 전달
+        return new SportsStore("나이키", 10, discountStrategy);
     }
 }
--- src/main/java/designPattern/store/model/stores/SportsStore.java
+++ src/main/java/designPattern/store/model/stores/SportsStore.java
@@ -1,39 +1,65 @@
 package designPattern.store.model.stores;
 
 import designPattern.store.items.Item;
+import designPattern.store.model.interfaces.DiscountStrategy;
 import designPattern.store.model.interfaces.Store;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SportsStore implements Store {
     private String name;
     private int capacity;
     private boolean isOpen;
+    private DiscountStrategy discountStrategy;
+    private Map<String, Item> inventory; // 가게의 재고를 나타냅니다.
 
-    public SportsStore(String name, int capacity, boolean isOpen) {
+    public SportsStore(String name, int capacity, DiscountStrategy discountStrategy) {
         this.name = name;
         this.capacity = capacity;
-        this.isOpen = isOpen;
+        this.isOpen = false; // 기본적으로 가게는 닫힌 상태입니다.
+        this.discountStrategy = discountStrategy;
+        this.inventory = new HashMap<>();
     }
 
+    // 아이템 구매
     public boolean purchaseItem(Item item, int quantity) {
+        // 재고 확인 로직 (여기서는 단순화하여 재고 체크를 생략합니다)
+
+        // 할인 로직 적용
+        double discountedPrice = discountStrategy.applyDiscount(item.getPrice()) * quantity;
+        System.out.println(item.getName() + " 상품을 " + discountedPrice + "원에 구매하셨습니다.");
+
+        // 재고 업데이트 로직 (여기서는 단순화하여 재고 업데이트를 생략합니다)
+
         return true;
     }
 
+    // 할인 가능 여부
     public boolean isSaleAllowed() {
-        return true;
+        return true; // 스포츠 매장에서는 할인이 가능합니다.
     }
 
+    // 재고 아이템 추가
+    public void addItemToInventory(Item item) {
+        inventory.put(item.getId(), item);
+    }
+
+    // 가게 개점
     @Override
     public void openStore() {
         isOpen = true;
-        // 추가적인 오픈 로직...
+        System.out.println(name + " 매장이 열렸습니다.");
     }
 
+    // 가게 폐점
     @Override
     public void closeStore() {
         isOpen = false;
-        // 추가적인 마감 로직...
+        System.out.println(name + " 매장이 닫혔습니다.");
     }
 
+    // Getter 메소드들
     @Override
     public String getName() {
         return name;
@@ -50,4 +76,3 @@ public class SportsStore implements Store {
     }
 
 }
-
