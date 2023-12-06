d71bb756bb0c98f8bab0615a9e463ce9e2c747cc
권장순 <jsoonworld@gmail.com>
2023-12-06 19:41:57 +0900

    feat( SelectionStoreInputController ) : 생성할 상점을 입력 받는 기능을 관리하는 컨트롤러 구현.

--- src/main/java/designPattern/store/v2/controller/SelectionStoreInputController.java
+++ src/main/java/designPattern/store/v2/controller/SelectionStoreInputController.java
@@ -1,4 +1,33 @@
 package designPattern.store.v2.controller;
 
+import designPattern.store.v2.view.input.SelectionStoreInputView;
+import designPattern.store.v2.view.output.SelectionStoreOutputView;
+import designPattern.store.v2.util.SelectionStoreInputValidator;
+
 public class SelectionStoreInputController {
+
+    private SelectionStoreOutputView outputView;
+    private SelectionStoreInputView inputView;
+
+    public SelectionStoreInputController(SelectionStoreOutputView outputView, SelectionStoreInputView inputView) {
+        this.outputView = outputView;
+        this.inputView = inputView;
+    }
+
+    public void processUserInput() {
+        String input;
+        boolean isValid;
+
+        do {
+            outputView.displayWelcomeMessage();
+            input = inputView.receiveUserInput();
+            isValid = SelectionStoreInputValidator.isInputValid(input);
+
+            if (!isValid) {
+                outputView.displayErrorMessage();
+            }
+        } while (!isValid);
+
+        outputView.displayInputResult(input);
+    }
 }
