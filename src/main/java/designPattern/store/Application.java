f416e56c8386ef3d3c8a96d83b0c555667aa415c
권장순 <jsoonworld@gmail.com>
2023-12-07 13:59:05 +0900

    refactoring : 전체적인 구조를 재정립. version1, 2에서는 store의 설정부터 방향이 잘못 흘러가.

--- src/main/java/designPattern/store/Application.java
+++ src/main/java/designPattern/store/Application.java
@@ -1,10 +1,8 @@
 package designPattern.store;
 
 
-import designPattern.store.v2.controller.SelectionStoreInputController;
 
 public class Application {
     public static void main(String[] args) {
-        SelectionStoreInputController.startInputProcess();
     }
 }
