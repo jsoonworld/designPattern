79f86bd965d8b66645afbde4d57f51f8211e18f2
권장순 <jsoonworld@gmail.com>
2023-12-07 14:25:46 +0900

    feat( store ) : 매장 공통 기능을 담은 매장 인터페이스 구현.

--- src/main/java/designPattern/store/v3/model/store/Store.java
+++ src/main/java/designPattern/store/v3/model/store/Store.java
@@ -2,4 +2,15 @@ package designPattern.store.v3.model.store;
 
 public interface Store {
 
+    void displayStoreInfo();
+    void setOperatingHours(String openingTime, String closingTime);
+    String getOperatingHours();
+
+    ContractYear getContractYear();
+
+    Size getSize();
+
+    Rent getRent();
+
+    Location getLocation();
 }
