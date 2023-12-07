e1307a75ae72aea7da653a6f47c2cc62cc4351bc
권장순 <jsoonworld@gmail.com>
2023-12-07 15:49:11 +0900

    feat( Store ) : store interface 구현.

--- src/main/java/designPattern/store/v4/view/model/Store.java
+++ src/main/java/designPattern/store/v4/view/model/Store.java
@@ -1,5 +1,15 @@
 package designPattern.store.v4.view.model;
 
 public interface Store {
+    void setContractYear(ContractYear contractYear);
+    void setSize(Size size);
+    void setRent(Rent rent);
+    void setLocation(Location location);
 
+    ContractYear getContractYear();
+    Size getSize();
+    Rent getRent();
+    Location getLocation();
+
+    void displayStoreInfo();
 }
