856bfc75f1f5a9a2914b8517da3b6f7c0f2ac50b
권장순 <jsoonworld@gmail.com>
2023-12-07 14:38:03 +0900

    feat( LuxuryStore ) : 럭셔리 매장 틀 구현.

--- src/main/java/designPattern/store/v3/controller/factories/StoreFactory.java
+++ src/main/java/designPattern/store/v3/controller/factories/StoreFactory.java
@@ -1,14 +1,17 @@
 package designPattern.store.v3.controller.factories;
 
+import designPattern.store.v3.model.BrandCharacteristic;
+import designPattern.store.v3.model.luxuryBrand.LuxuryBrand;
 import designPattern.store.v3.model.store.*;
 
 public class StoreFactory {
-    public static Store createStore(String type, ContractYear year, Size size, Rent rent, Location location) {
+    public static Store createStore(String type, ContractYear year, Size size, Rent rent, Location location, LuxuryBrand brand, BrandCharacteristic characteristic) {
         switch (type) {
             case "Luxury":
-                return new LuxuryStore(year, size, rent, location);
+                return new LuxuryStore(year, size, rent, location, brand, characteristic);
             default:
                 throw new IllegalArgumentException("[ERROR] 유요하지 않은 타입입니다.: " + type + ")");
         }
     }
 }
+
