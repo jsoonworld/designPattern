8e57898f1a640811f7afe168cd8d00d31ea59f36
권장순 <jsoonworld@gmail.com>
2023-12-08 22:49:56 +0900

    feat( view ) main에서 view 분리.

--- src/main/java/designPattern/store/test2/controller/StoreCreationController.java
+++ src/main/java/designPattern/store/test2/controller/StoreCreationController.java
@@ -3,16 +3,21 @@ package designPattern.store.test2.controller;
 import designPattern.store.test2.model.Store;
 import designPattern.store.test2.model.StoreFactory;
 import designPattern.store.test2.model.StoreType;
-import designPattern.store.test2.view.StoreView;
+import designPattern.store.test2.view.InputView;
+import designPattern.store.test2.view.OutputView;
 
 public class StoreCreationController {
-    private StoreView view;
+    private InputView inputView;
+    private OutputView outputView;
 
-    public StoreCreationController(StoreView view) {
-        this.view = view;
+    public StoreCreationController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public Store createStore(StoreType storeType, int contractYears, double size, double rent, String location) {
+        // 여기서 inputView와 outputView를 사용할 수도 있고,
+        // Store의 생성 로직에만 집중할 수도 있습니다.
         Store.StoreBuilder builder = new Store.StoreBuilder()
                 .setContractYears(contractYears)
                 .setSize(size)
--- src/main/java/designPattern/store/test2/main/Application.java
+++ src/main/java/designPattern/store/test2/main/Application.java
@@ -2,34 +2,36 @@ package designPattern.store.test2.main;
 
 
 import designPattern.store.test2.model.Store;
-import designPattern.store.test2.view.StoreView;
+import designPattern.store.test2.view.InputView;
+import designPattern.store.test2.view.OutputView;
 import designPattern.store.test2.controller.BrandSelectionController;
 import designPattern.store.test2.controller.StoreCreationController;
 import designPattern.store.test2.model.StoreType;
 
 public class Application {
     public static void main(String[] args) {
-        StoreView view = new StoreView();
-        StoreCreationController storeCreationController = new StoreCreationController(view);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StoreCreationController storeCreationController = new StoreCreationController(inputView, outputView);
         BrandSelectionController brandSelectionController = new BrandSelectionController();
 
         // 매장 유형 및 속성 입력 받기
-        StoreType storeType = StoreType.valueOf(view.getInput("Enter store type (LUXURY, SPORTS, PC_ROOM): ").toUpperCase());
-        int contractYears = Integer.parseInt(view.getInput("Enter contract years: "));
-        double size = Double.parseDouble(view.getInput("Enter size (sqm): "));
-        double rent = Double.parseDouble(view.getInput("Enter rent: "));
-        String location = view.getInput("Enter location: ");
+        StoreType storeType = StoreType.valueOf(inputView.getInput("Enter store type (LUXURY, SPORTS, PC_ROOM): ").toUpperCase());
+        int contractYears = Integer.parseInt(inputView.getInput("Enter contract years: "));
+        double size = Double.parseDouble(inputView.getInput("Enter size (sqm): "));
+        double rent = Double.parseDouble(inputView.getInput("Enter rent: "));
+        String location = inputView.getInput("Enter location: ");
 
         // 매장 생성 및 브랜드 선택
         Store store = storeCreationController.createStore(storeType, contractYears, size, rent, location);
         String brand = brandSelectionController.selectBrand(storeType);
 
         // 결과 출력
-        view.displayMessage("Selected Brand: " + brand);
-        view.displayMessage("Created a " + storeType + " store with the following details:");
-        view.displayMessage("Contract Years: " + store.getContractYears());
-        view.displayMessage("Size: " + store.getSize() + " sqm");
-        view.displayMessage("Rent: " + store.getRent() + " per month");
-        view.displayMessage("Location: " + store.getLocation());
+        outputView.displayMessage("Selected Brand: " + brand);
+        outputView.displayMessage("Created a " + storeType + " store with the following details:");
+        outputView.displayMessage("Contract Years: " + store.getContractYears());
+        outputView.displayMessage("Size: " + store.getSize() + " sqm");
+        outputView.displayMessage("Rent: " + store.getRent() + " per month");
+        outputView.displayMessage("Location: " + store.getLocation());
     }
 }
