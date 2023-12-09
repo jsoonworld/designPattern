aed89f093858d1e5c188c20119d7379e85f67f86
권장순 <jsoonworld@gmail.com>
2023-12-09 16:05:42 +0900

    feat( Application ) : main에 적용.

--- src/main/java/designPattern/store/Application.java
+++ src/main/java/designPattern/store/Application.java
@@ -1,6 +1,7 @@
 package designPattern.store;
 
 import designPattern.store.v9.controller.*;
+import designPattern.store.v9.controller.factory.ControllerFactory;
 import designPattern.store.v9.controller.selection.BrandSelectionController;
 import designPattern.store.v9.controller.selection.LocationSelectionController;
 import designPattern.store.v9.model.store.Store;
@@ -12,11 +13,12 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        StoreCreationController storeCreationController = new StoreCreationController(inputView, outputView);
-        BrandSelectionController brandSelectionController = new BrandSelectionController(inputView);
-        LocationSelectionController locationSelectionController = new LocationSelectionController(inputView);
-        StoreInputController storeInputController = new StoreInputController(inputView, locationSelectionController);
-        StoreResultController storeResultController = new StoreResultController(outputView);
+        ControllerFactory controllerFactory = new ControllerFactory(inputView, outputView);
+
+        StoreCreationController storeCreationController = controllerFactory.createStoreCreationController();
+        BrandSelectionController brandSelectionController = controllerFactory.createBrandSelectionController();
+        StoreInputController storeInputController = controllerFactory.createStoreInputController();
+        StoreResultController storeResultController = controllerFactory.createStoreResultController();
 
         // 매장 유형 및 속성 입력 받기
         StoreCreationData storeData = storeInputController.getStoreCreationData();
