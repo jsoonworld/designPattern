82a8e2f08534a3d94e7cd6763689b839c1ed2ee7
권장순 <jsoonworld@gmail.com>
2023-12-09 15:04:57 +0900

    feat( StoreInputController ) : 입력 정보를 담당하는 controller 구현.

---
+++ src/main/java/designPattern/store/test2/controller/StoreInputController.java
@@ -0,0 +1,34 @@
+package designPattern.store.test2.controller;
+
+import designPattern.store.test2.model.StoreType;
+import designPattern.store.test2.view.InputView;
+
+public class StoreInputController {
+    private InputView inputView;
+    private LocationSelectionController locationSelectionController;
+
+    public StoreInputController(InputView inputView, LocationSelectionController locationSelectionController) {
+        this.inputView = inputView;
+        this.locationSelectionController = locationSelectionController;
+    }
+
+    public StogreType getStoreType() {
+        return StoreType.valueOf(inputView.getStoreTypeInput().toUpperCase());
+    }
+
+    public int getContractYears() {
+        return Integer.parseInt(inputView.getContractYearsInput());
+    }
+
+    public double getSize() {
+        return Double.parseDouble(inputView.getSizeInput());
+    }
+
+    public double getRent() {
+        return Double.parseDouble(inputView.getRentInput());
+    }
+
+    public String getLocation() {
+        return locationSelectionController.selectLocation();
+    }
+}
