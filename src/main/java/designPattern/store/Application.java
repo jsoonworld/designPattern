5569a81932c11ec048910ae33e38fe09cfa5af4e
권장순 <jsoonworld@gmail.com>
2023-12-09 16:09:59 +0900

    refactoring ( Application ) : 구체적인 코드가 들어나지 않게 개선.

--- src/main/java/designPattern/store/Application.java
+++ src/main/java/designPattern/store/Application.java
@@ -1,9 +1,6 @@
 package designPattern.store;
 
-import designPattern.store.v9.controller.*;
 import designPattern.store.v9.controller.factory.ControllerFactory;
-import designPattern.store.v9.controller.selection.BrandSelectionController;
-import designPattern.store.v9.controller.selection.LocationSelectionController;
 import designPattern.store.v9.model.store.Store;
 import designPattern.store.v9.model.store.StoreCreationData;
 import designPattern.store.v9.view.InputView;
@@ -15,21 +12,16 @@ public class Application {
         OutputView outputView = new OutputView();
         ControllerFactory controllerFactory = new ControllerFactory(inputView, outputView);
 
-        StoreCreationController storeCreationController = controllerFactory.createStoreCreationController();
-        BrandSelectionController brandSelectionController = controllerFactory.createBrandSelectionController();
-        StoreInputController storeInputController = controllerFactory.createStoreInputController();
-        StoreResultController storeResultController = controllerFactory.createStoreResultController();
-
         // 매장 유형 및 속성 입력 받기
-        StoreCreationData storeData = storeInputController.getStoreCreationData();
+        StoreCreationData storeData = controllerFactory.createStoreInputController().getStoreCreationData();
 
         // 매장 생성
-        Store store = storeCreationController.createStore(storeData);
+        Store store = controllerFactory.createStoreCreationController().createStore(storeData);
 
         // 브랜드 선택
-        String brand = brandSelectionController.selectBrand(store.getStoreType());
+        String brand = controllerFactory.createBrandSelectionController().selectBrand(store.getStoreType());
 
         // 결과 출력
-        storeResultController.displayStoreDetails(store, store.getStoreType(), brand);
+        controllerFactory.createStoreResultController().displayStoreDetails(store, store.getStoreType(), brand);
     }
 }
