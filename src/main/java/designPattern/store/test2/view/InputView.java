16eecf119415f23d5bc10859b015cdb2b09fb55a
권장순 <jsoonworld@gmail.com>
2023-12-09 15:03:59 +0900

    feat( InputView ) : main에서 받은 input을 view로 이전.

--- src/main/java/designPattern/store/test2/view/InputView.java
+++ src/main/java/designPattern/store/test2/view/InputView.java
@@ -9,4 +9,24 @@ public class InputView {
         System.out.print(prompt);
         return scanner.nextLine();
     }
+
+    // 매장 유형 입력 받기
+    public String getStoreTypeInput() {
+        return getInput("Enter store type (LUXURY, SPORTS, PC_ROOM): ");
+    }
+
+    // 계약 연수 입력 받기
+    public String getContractYearsInput() {
+        return getInput("Enter contract years: ");
+    }
+
+    // 크기 입력 받기
+    public String getSizeInput() {
+        return getInput("Enter size (sqm): ");
+    }
+
+    // 임대료 입력 받기
+    public String getRentInput() {
+        return getInput("Enter rent: ");
+    }
 }
